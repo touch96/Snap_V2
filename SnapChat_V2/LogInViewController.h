331e2938a3b426d43a483498d275cdb69d136d5e
shokolov <kimbyungyoon@KimByungyoon-no-MacBook-Pro.local>
2013-10-06 14:38:26 +0900

    all

---
+++ SnapChat_V2/LogInViewController.h
@@ -0,0 +1,16 @@
+//
+//  LogInViewController.h
+//  SnapChat
+//
+//  Created by KimByungyoon on 2013/09/08.
+//  Copyright (c) 2013年 KimByungyoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *account;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+
+@end
---
+++ SnapChat_V2/LogInViewController.m
@@ -0,0 +1,49 @@
+//
+//  LogInViewController.m
+//  SnapChat
+//
+//  Created by KimByungyoon on 2013/09/08.
+//  Copyright (c) 2013年 KimByungyoon. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+@synthesize account;
+@synthesize password;
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)doButtonDown:(id)sender {
+    NSLog(@"account:%@", account.text);
+    NSLog(@"password:%@", password.text);
+    [self performSegueWithIdentifier:@"cameraSegue" sender:self];
+    
+}
+
+@end
