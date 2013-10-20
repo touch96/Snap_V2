8bb3ddac5c906d39d840697d4ba7933e27884bbe
youngmo <youngmojp@yahoo.co.jp>
2013-10-19 23:23:46 -0700

    카메라 로직을 LoginViewController에서 분리

---
+++ SnapChat_V2/MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  SnapChat_V2
+//
+//  Created by A12325 on 2013/10/20.
+//  Copyright (c) 2013年 KimByungyoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface MainViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
